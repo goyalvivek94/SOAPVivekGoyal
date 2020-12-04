f45971a9bf79368ab284bb01cda675be35ce4cef
goyalvivek94 <goyalvivek94@gmail.com>
2020-12-04 13:16:01 +0530

    first commit

---
+++ src/main/java/RoshanBatch/App.java
@@ -0,0 +1,13 @@
+package RoshanBatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
